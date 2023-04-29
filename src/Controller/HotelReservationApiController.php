bfd3abddd40afa3e9fe3bcef868792f5d7b40395
miko7ajradziw1llowicz <miko7ajradziw1llowicz@gmail.com>
2023-04-29 11:21:21 +0200

    POST NOT WORKING GET WORKING COOL

--- src/Controller/HotelReservationApiController.php
+++ src/Controller/HotelReservationApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\HotelReservationType;
 use App\Entity\HotelReservation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,7 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
-
+namespace App\Controller;
+use App\Entity\HotelReservation;
+use App\Form\HotelReservationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 /**
  * @Route("/api/hotel-reservations")
  */
